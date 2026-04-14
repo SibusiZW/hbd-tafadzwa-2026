1c59505c9ea7a3a11e387592b1f6d9040f236270
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-04-14 20:46:44 +0200

    added whatsapp and Instagram links

--- src/app/hbd-card/hbd-card.ts
+++ src/app/hbd-card/hbd-card.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { Instagram, LucideAngularModule, MessageCircle } from 'lucide-angular';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-hbd-card',
-  imports: [LucideAngularModule],
+  imports: [LucideAngularModule, FormsModule],
   templateUrl: './hbd-card.html',
   styleUrl: './hbd-card.css',
 })
 export class HbdCard {
   readonly icons = { Instagram, MessageCircle }
   birthdayBoy = "Tafadzwa Sibanda"
+  message = ''
 }

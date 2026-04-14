f2d781abbf23e41cf90dfd9eafa99ef751ed2016
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-04-14 19:24:24 +0200

    succesfully setup Lucide in hbc-card component

--- src/app/hbd-card/hbd-card.ts
+++ src/app/hbd-card/hbd-card.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { Instagram, LucideAngularModule, MessageCircle } from 'lucide-angular';
 
 @Component({
   selector: 'app-hbd-card',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './hbd-card.html',
   styleUrl: './hbd-card.css',
 })
-export class HbdCard {}
+export class HbdCard {
+  readonly icons = { Instagram, MessageCircle }
+  birthdayBoy = "Tafadzwa Sibanda"
+}

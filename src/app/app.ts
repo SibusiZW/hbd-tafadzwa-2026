2aff1563e95a9315f84c43b0e4624e4fad887ee0
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-04-14 19:03:56 +0200

    added styling for page component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { HbdCard } from './hbd-card/hbd-card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HbdCard],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('hbd-tafadzwa-2026');
+  
 }
